948cf548ccfb6a6916ec25e8bf20bbd2414ed6bf
Harshita Sharma <harshitasharma5801@gmail.com>
2026-05-24 10:36:03 +0530

    Fibonacci

--- Loops/Pattern_loop.py
+++ Loops/Pattern_loop.py
@@ -7,4 +7,13 @@ for i in range(1,6):
 for i  in range(4,0,-1):
     for j in range (i):
         print("*",end=" ")
-    print()        
+    print()    
+    
+    
+             #or
+             
+a = int(input("Enter the no of time you want star to get print: "))
+for i in range (1,a+1):
+    print("*" * i)
+for i in range (a-1 , 0 , -1):
+    print("*" *i)                 

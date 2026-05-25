24ccc1a9720479eb2472f13d4fa34d25d92f7573
Harshita Sharma <harshitasharma5801@gmail.com>
2026-05-25 20:12:41 +0530

    Bool_Practice

---
+++ Basics/Boolean/Bool.py
@@ -0,0 +1,9 @@
+is_logged_in =True
+has_permission =False
+print("is_logged_in",is_logged_in,"type= ",type(is_logged_in))
+print("has_permission",has_permission,"type= ",type(has_permission))
+print(is_logged_in and has_permission)
+print(is_logged_in or has_permission)
+print(not is_logged_in)
+print(not has_permission)
+
---
+++ Basics/Boolean/Bool_practise1.py
@@ -0,0 +1,7 @@
+marks=int(input("enter your marks= "))
+is_pass = marks >= 50
+is_distinction = marks >= 75
+is_fail = marks < 50
+print(is_pass)
+print(is_distinction)
+print(is_fail)

1ce439620b3baa18c1641abffeb453ea608881e5
Harshita Sharma <harshitasharma5801@gmail.com>
2026-05-25 19:37:01 +0530

    Control_flow_practice

--- Control flow/Armstrong_or_not.py
+++ Control flow/Armstrong_or_not.py
@@ -2,8 +2,8 @@ num=int(input("enter a no= " ))
 original=num
 sum=0
 while num>0:
-    digit=num%10
-    sum=sum+digit**3
+    rem=num%10
+    sum=sum+rem**3
     num=num//10
 if original==sum:
         print("armstrong")
---
+++ Control flow/Student_marks.py
@@ -0,0 +1,7 @@
+marks=int(input("enter marks= "))
+if marks>=40:
+    print("pass")
+elif marks<40:
+    print("fail")
+else:
+    print("null")        
